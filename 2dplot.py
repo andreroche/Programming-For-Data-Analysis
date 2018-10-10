58411bca906a6b097c029649f90ee1dbd0f12e51
André Roche <36365088+andreroche@users.noreply.github.com>
2018-10-10 18:42:46 +0100

    Add files via upload

---
+++ 2dplot.py
@@ -0,0 +1,10 @@
+ #  pyplot tutorial from matplotlibtutorial.org. André Roche 10-Oct-18. GMIT course videos 
+
+import matplotlib.pyplot as plt  # import the plot package. the plt is just a make upey word/ nickname
+
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'b.')  # b. means blue dot on the graph 
+plt.ylabel ("Some numbers")
+
+plt.show()
+
+
